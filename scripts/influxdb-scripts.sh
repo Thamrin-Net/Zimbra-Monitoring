d0ffd8060b43a58c6c9f63f76fb7e180c22085f2
I-Fun <i.fuunn@gmail.com>
2023-10-18 20:41:52 +0700

    Update influxdb-scripts.sh
    before simplify the account status

--- scripts/influxdb-scripts.sh
+++ scripts/influxdb-scripts.sh
@@ -3,92 +3,74 @@
 # Script By : I-Fun | 20231016
 
 # --------------- TOP 10 SENDER ------------------------------------
-
-topsender=$(cat /var/log/zimbra.log | awk -F 'from=<' '{print $2}' | awk -F'>' '{print $1}' | sed '/^$/d'| grep -v "bounces" | sort | uniq -c | sort -nk1 -r | sed -n '1,10p')
-
+topsender=$(cat /var/log/zimbra.log | awk -F 'from=<' '{print $2}' | awk -F'>' '{print $1}' | sed '/^$/d'| grep -v "bounce" | sort | uniq -c | sort -nk1 -r | sed -n '1,10p')
 # Process the data line by line
 while IFS= read -r line; do
   # Skip empty lines
   if [[ -z "$line" ]]; then
     continue
   fi
-
   # Extract domain and status values
   sent=$(echo "$line" | awk '{print $1}')
   email=$(echo "$line" | awk '{print $2}')
-
   # Print the Influxdb-style
   echo "top_sender,email=$email total=$sent"
 done <<< "$topsender"
 
 # --------------- TOP 10 RECEIVER ------------------------------------
-
-topreceiver=$(cat /var/log/zimbra.log | awk -F 'to=<' '{print $2}' | awk -F'>' '{print $1}' | sed '/^$/d'| grep -v "bounces" | sort | uniq -c | sort -nk1 -r | sed -n '1,10p')
-
+topreceiver=$(cat /var/log/zimbra.log | awk -F 'to=<' '{print $2}' | awk -F'>' '{print $1}' | sed '/^$/d'| grep -v "bounce" | sort | uniq -c | sort -nk1 -r | sed -n '1,10p')
 # Process the data line by line
 while IFS= read -r line; do
   # Skip empty lines
   if [[ -z "$line" ]]; then
     continue
   fi
-
   # Extract domain and status values
   receive=$(echo "$line" | awk '{print $1}')
   email=$(echo "$line" | awk '{print $2}')
-
   # Print the Influxdb-style
   echo "top_receiver,email=$email total=$receive"
 done <<< "$topreceiver"
 
 # --------------- TOP 10 REJECTED MAIL SERVER ------------------------------------
-
 toprejectsrv=$(cat /var/log/zimbra.log | grep reject | awk -F '<' '{print $2}' | awk -F '>' '{print $1}' | sed '/^$/d'| sort | uniq -c | sort -nk1 -r | sed -n '1,10p')
-
 # Process the data line by line
 while IFS= read -r line; do
   # Skip empty lines
   if [[ -z "$line" ]]; then
     continue
   fi
-
   # Extract domain and status values
   reject=$(echo "$line" | awk '{print $1}')
   host=$(echo "$line" | awk '{print $2}')
-
   # Print the Influxdb-style
-  echo "top_reject_server,host=$host total=$reject"
+  echo "top_reject_server,servername=$host total=$reject"
 done <<< "$toprejectsrv"
 
 # --------------- TOP 10 REJECTED SENDER ------------------------------------
-
 toprejectsender=$(cat /var/log/zimbra.log | grep reject | awk -F 'from=<' '{print $2}' | awk -F '>' '{print $1}' | sed '/^$/d'| sort | uniq -c | sort -nk1 -r | sed -n '1,10p')
-
 # Process the data line by line
 while IFS= read -r line; do
   # Skip empty lines
   if [[ -z "$line" ]]; then
     continue
   fi
-
   # Extract domain and status values
   reject=$(echo "$line" | awk '{print $1}')
   sender=$(echo "$line" | awk '{print $2}')
-
   # Print the Influxdb-style
   echo "top_reject_sender,sender=$sender total=$reject"
 done <<< "$toprejectsender"
 
-# -------------- DOMAIN STATUS -------------------------------------
-
-domain_status=$(su - zimbra -c '/opt/zimbra/bin/zmaccts' | grep -v "spam." | grep -v "virus-quarantine." | awk '/domain summary/,0' | tail -n +5)
-
+# -------------- Account Status -------------------------------------
+# Account on Domain Status (active, closed, locked, maintenance, total)
+account_status=$(su - zimbra -c '/opt/zimbra/bin/zmaccts' | grep -v "spam." | grep -v "virus-quarantine." | awk '/domain summary/,0' | tail -n +5)
 # Process the data line by line
 while IFS= read -r line; do
   # Skip empty lines
   if [[ -z "$line" ]]; then
     continue
   fi
-
   # Extract domain and status values
   domain=$(echo "$line" | awk '{print $1}')
   active=$(echo "$line" | awk '{print $2}')
@@ -96,42 +78,72 @@ while IFS= read -r line; do
   locked=$(echo "$line" | awk '{print $4}')
   mainte=$(echo "$line" | awk '{print $5}')
   total=$(echo "$line" | awk '{print $6}')
-
-
   # Print the Influxdb-style
-  echo "domain_status,domain=$domain active=$active"
-  echo "domain_status,domain=$domain closed=$closed"
-  echo "domain_status,domain=$domain locked=$locked"
-  echo "domain_status,domain=$domain maintenance=$mainte"
-  echo "domain_status,domain=$domain total=$total"
-done <<< "$domain_status"
+  echo "account_status,domain=$domain active=$active"
+  echo "account_status,domain=$domain closed=$closed"
+  echo "account_status,domain=$domain locked=$locked"
+  echo "account_status,domain=$domain maintenance=$mainte"
+  echo "account_status,domain=$domain total=$total"
+done <<< "$account_status"
 
-# ----------- ACCOUNT SIZE USAGE -----------------------------------
+# Total Account on Mail Server
+account_status_total=$(su - zimbra -c '/opt/zimbra/bin/zmaccts' |
+grep -v "spam." |
+grep -v "virus-quarantine." |
+awk '/domain summary/,0' |
+tail -n +5 |
+awk '{print $2, $3, $4, $5, $6}' |
+awk '{a+=$1; c+=$2; l+=$3; m+=$4; t+=$5} END {print a, c, l, m, t}')
+# Process Data column by column
+read a c l m t <<< "$account_status_total"
+echo "account_status,domain=all active=$a"
+echo "account_status,domain=all closed=$c"
+echo "account_status,domain=all locked=$l"
+echo "account_status,domain=all maintenance=$m"
+echo "account_status,domain=all total=$t"
+# Account Lockout Status
+account_status_lockout_total=$(su - zimbra -c '/opt/zimbra/bin/zmaccts' | grep lockout | wc -l | awk '{lockout=$1} END {print lockout}')
+read lockout <<< $account_status_lockout_total
+echo "account_status,domain=all lockout=$lockout"
 
-MAILSERVER=$(/opt/zimbra/bin/zmhostname)
-TOP=10
-account_usage=$(su - zimbra -c "zmprov getQuotaUsage $MAILSERVER | grep -v 'spam.' | grep -v 'virus-quarantine.' | head -n $TOP")
-
+# Account with Status Lockout
+account_status_lockout=$(su - zimbra -c '/opt/zimbra/bin/zmaccts' | grep lockout)
 # Process the data line by line
 while IFS= read -r line; do
   # Skip empty lines
   if [[ -z "$line" ]]; then
     continue
   fi
+  # Extract account and last sign in values
+  username=$(echo "$line" | awk '{print $1}')
+  lastsign="$(echo "$line" | awk '{print $5}' | awk '{gsub(/\./, "", $0); print}' | awk '{ gsub("never", "0"); print }')"
+  formatted_date="$(date -d "$(echo "$lastsign" | sed -r 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/')" '+%Y-%m-%d %H:%M:%S')"
+  lastsign="$(echo "$formatted_date" | awk '{ if ($2 == "00:00:00") $1 = "Never"; print $1, $2 }')"
+  # Print the Influxdb-style
+  echo "account_status,username=$username lastsignin=\"$lastsign\""
 
+done <<< "$account_status_lockout"
+
+# ----------- ACCOUNT SIZE USAGE -----------------------------------
+MAILSERVER=$(/opt/zimbra/bin/zmhostname)
+TOP=10
+account_usage=$(su - zimbra -c "zmprov getQuotaUsage $MAILSERVER | grep -v 'spam.' | grep -v 'virus-quarantine.' | head -n $TOP")
+# Process the data line by line
+while IFS= read -r line; do
+  # Skip empty lines
+  if [[ -z "$line" ]]; then
+    continue
+  fi
   # Extract domain and status values
   account=$(echo "$line" | awk '{print $1}')
   quota=$(echo "$line" | awk '{print $2}')
   usage=$(echo "$line" | awk '{print $3}')
-
-
   # Print the Influxdb-style
   echo "account_details,account=$account usage=$usage"
   echo "account_details,account=$account quota=$quota"
 done <<< "$account_usage"
 
 # ------------ ZIMBRA STATUS -------------------------------------
-
 get_sv=$(su - zimbra -c "/opt/zimbra/bin/zmcontrol status")
 IFS=$'\n'
 get_sv=($get_sv)
@@ -156,4 +168,3 @@ for i in "${!get_sv[@]}"; do
 
   fi
 done
-
