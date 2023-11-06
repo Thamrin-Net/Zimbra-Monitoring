a4abd317b64a998bac7c3814edfb9bcb707ec4ee
I-Fun <i.fuunn@gmail.com>
2023-11-06 23:25:07 +0700

    Update git-clone.sh

--- git-clone.sh
+++ git-clone.sh
@@ -3,4 +3,5 @@ git clone https://github.com/Thamrin-Net/Zimbra-Monitoring /tmp/zmbmon
 mv /tmp/zmbmon/* /etc/telegraf/
 rm -rf /etc/telegraf/README.md
 rm -rf /etc/telegraf/scripts/Readme.md
+rm -rf /etc/telegraf/git-clone.sh
 rm -rf /tmp/zmbmon
